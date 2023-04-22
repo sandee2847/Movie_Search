3f9373899aad809048cda0aa89969754d82255cf
sandee2847 <fugiganiyar14@gmail.com>
2023-04-22 15:28:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  function handleSearchQueryChange(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  function handleSearchQuerySubmit(event) {
+    event.preventDefault();
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=6f1fd080d76e7e88d084ca557506456a&query=${searchQuery}`)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results))
+      .catch((error) => console.error(error));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        <h1>Movie Search</h1>
+        <form onSubmit={handleSearchQuerySubmit}>
+          <label>
+            Search for movies:
+            <input type="text" value={searchQuery} onChange={handleSearchQueryChange} />
+          </label>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      {movies.map((movie) => (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }} key={movie.id}>
+          <h2>{movie.title}</h2>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+          <p>{movie.overview}</p>
+        </div>
+      ))}
     </div>
   );
 }
